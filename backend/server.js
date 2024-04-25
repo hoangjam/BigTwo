b46c87cf92c4339235ee5c718e8c6ec8f1126d73
hoangjam <j.hoang2080@gmail.com>
2024-04-25 19:25:52 -0400

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
+require('dotenv').config()
+
 const express = require('express');
 
 // express app
 const app = express();
 
+// this is the middleware, showing path
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+
+    next() // next function middleware
+});
+
+// router handler, responds to get request
+app.get('/', (req, res) => {
+    res.json({msg: "Big 2"})
+
+});
+
 // listen for requests aka a port number
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
